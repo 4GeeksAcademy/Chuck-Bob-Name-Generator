0f5464b755836ffcd71ed4ee72701513407da2df
Chuck-Bob <derekaassaf@gmail.com>
2024-10-15 17:01:51 +0000

    first upload

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let exten = [".us", ".com", ".org", ".net"];
+  let names = [];
+
+  for (let p of pronoun) {
+    for (let a of adj) {
+      for (let n of noun) {
+        for (let e of exten) {
+          console.log(p + a + n + e);
+          document.querySelector(".mt-5").innerHTML +=
+            "<h2>" + p + a + n + e + "</h2>";
+          //   names.push(p + a + n + e);
+        }
+      }
+    }
+  }
+
+  // document.querySelector(".mt-5").innerHTML = "<h2>" + names + "</h2>";
 };
